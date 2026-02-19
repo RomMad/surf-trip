7e3f620fa7e180aa7885ccbafa7667973b34d803
RomMad <romain.madelaine@gmail.com>
2026-02-19 16:51:19 +0100

    feat: add symfony/ux-toolkit package and register its bundle

--- config/bundles.php
+++ config/bundles.php
@@ -17,4 +17,5 @@ return [
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
     Symfony\UX\TwigComponent\TwigComponentBundle::class => ['all' => true],
     Symfony\UX\Icons\UXIconsBundle::class => ['all' => true],
+    Symfony\UX\Toolkit\UXToolkitBundle::class => ['dev' => true, 'test' => true],
 ];
