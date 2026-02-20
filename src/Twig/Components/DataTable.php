a69a0a01d23a4c40550f608e80307bfbd6da6b7a
RomMad <romain.madelaine@gmail.com>
2026-02-20 17:16:58 +0100

    feat: implement DataTable component and refactor IndexTable to use it

---
+++ src/Twig/Components/DataTable.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Twig\Components;
+
+use Symfony\UX\TwigComponent\Attribute\AsTwigComponent;
+use Symfony\UX\TwigComponent\Attribute\ExposeInTemplate;
+
+#[AsTwigComponent]
+final class DataTable
+{
+    /** @var list<string> */
+    public array $headers = [];
+
+    /** @var list<array<mixed>> */
+    public array $data = [];
+
+    #[ExposeInTemplate('empty_message')]
+    public string $emptyMessage = 'no_records_found.label';
+}
