7680f60bdae8f2969d115be05d6a0c1c6966eed6
RomMad <romain.madelaine@gmail.com>
2026-03-03 15:04:48 +0100

    fix: add current user as trip owner in new trip creation

--- src/Controller/Trip/NewTripController.php
+++ src/Controller/Trip/NewTripController.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Controller\Trip;
 
 use App\Entity\Trip;
+use App\Entity\User;
 use App\Enum\User\UserRole;
 use App\Form\TripFormType;
 use App\Repository\TripRepository;
@@ -12,6 +13,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\CurrentUser;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 final class NewTripController extends AbstractController
@@ -26,9 +28,11 @@ final class NewTripController extends AbstractController
         methods: [Request::METHOD_GET, Request::METHOD_POST]
     )]
     #[IsGranted(UserRole::USER)]
-    public function __invoke(Request $request): Response
+    public function __invoke(Request $request, #[CurrentUser()] User $currentUser): Response
     {
         $trip = new Trip();
+        $trip->addOwner($currentUser);
+
         $form = $this->createForm(TripFormType::class, $trip);
         $form->handleRequest($request);
 
