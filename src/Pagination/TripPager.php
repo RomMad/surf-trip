790090e3d701edf1a33b8ce36222b2c1299fdce1
RomMad <romain.madelaine@gmail.com>
2026-03-20 18:10:40 +0100

    feat: implement caching for pagination in TripPager

--- src/Pagination/TripPager.php
+++ src/Pagination/TripPager.php
@@ -9,12 +9,21 @@ use App\ReadModel\Trip\TripIndexReadModel;
 use App\Repository\TripRepository;
 use Pagerfanta\Pagerfanta;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\String\Slugger\SluggerInterface;
+use Symfony\Contracts\Cache\ItemInterface;
+use Symfony\Contracts\Cache\TagAwareCacheInterface;
 
 final readonly class TripPager
 {
+    public const string CACHE_TAG = 'trip.pagination';
+    private const string CACHE_KEY_PATTERN = 'trip.pagination.%s';
+    private const string CACHE_TTL = 'PT5M';
+
     public function __construct(
         private TripRepository $tripRepository,
         private PagerFactory $pagerFactory,
+        private SluggerInterface $slugger,
+        private TagAwareCacheInterface $cache,
     ) {}
 
     /**
@@ -22,14 +31,35 @@ final readonly class TripPager
      */
     public function create(TripFilter $filter, Request $request, int $maxPerPage = 10): Pagerfanta
     {
-        $queryBuilder = $this->tripRepository->createOrderedQueryBuilder($filter);
-        $countQueryBuilder = $this->tripRepository->getCountQueryBuilder($filter);
+        return $this->cache->get(
+            $this->generateCacheKey($request),
+            function (ItemInterface $item) use ($filter, $request, $maxPerPage): Pagerfanta {
+                $item->tag(self::CACHE_TAG);
+                $item->expiresAfter(new \DateInterval(self::CACHE_TTL));
 
-        return $this->pagerFactory->createWithCountQueryBuilder(
-            $queryBuilder,
-            $countQueryBuilder,
-            $request,
-            $maxPerPage,
+                $queryBuilder = $this->tripRepository->createOrderedQueryBuilder($filter);
+                $countQueryBuilder = $this->tripRepository->getCountQueryBuilder($filter);
+
+                return $this->pagerFactory->createWithCountQueryBuilder(
+                    $queryBuilder,
+                    $countQueryBuilder,
+                    $request,
+                    $maxPerPage,
+                );
+            },
         );
+    }
+
+    public function invalidate(): void
+    {
+        $this->cache->invalidateTags([self::CACHE_TAG]);
+    }
+
+    private function generateCacheKey(Request $request): string
+    {
+        $queryString = $request->getQueryString() ?? '';
+        $querySlug = $this->slugger->slug($queryString, '_')->toString();
+
+        return sprintf(self::CACHE_KEY_PATTERN, $querySlug);
     }
 }
