cba7f1529439dd5c4b629ff404d632666ab8329b
RomMad <romain.madelaine@gmail.com>
2026-05-05 17:32:24 +0200

    style: format setUpTest method parameters for better readability

--- tests/CustomWebTestCase.php
+++ tests/CustomWebTestCase.php
@@ -37,8 +37,12 @@ abstract class CustomWebTestCase extends WebTestCase
     /**
      * @param array<class-string<Story>>|class-string<Story> $stories
      */
-    protected function setUpTest(array|string $stories = [], ?string $username = null, bool $clearCache = true, bool $followRedirects = true): void
-    {
+    protected function setUpTest(
+        array|string $stories = [],
+        ?string $username = null,
+        bool $clearCache = true,
+        bool $followRedirects = true
+    ): void {
         parent::setUp();
 
         $this->client = static::createClient();
