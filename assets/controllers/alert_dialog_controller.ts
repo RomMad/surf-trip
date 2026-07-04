698771e5da6d2aed91266010e5a611d359962fbc
RomMad <romain.madelaine@gmail.com>
2026-07-04 20:21:12 +0200

    fix: change generic type of alert dialog controller to HTMLElement

--- assets/controllers/alert_dialog_controller.ts
+++ assets/controllers/alert_dialog_controller.ts
@@ -1,6 +1,6 @@
 import {Controller} from '@hotwired/stimulus';
 
-export default class extends Controller<HTMLDialogElement> {
+export default class extends Controller<HTMLElement> {
     static targets = ['trigger', 'dialog'];
 
     static values = {
