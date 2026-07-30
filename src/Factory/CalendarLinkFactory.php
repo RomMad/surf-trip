841604047504c4376f8f92d3273ef2d72d62ef19
RomMad <romain.madelaine@gmail.com>
2026-07-30 19:52:19 +0200

    refactor: update parameter syntax in CalendarLinkFactory for improved clarity

--- src/Factory/CalendarLinkFactory.php
+++ src/Factory/CalendarLinkFactory.php
@@ -12,9 +12,9 @@ final readonly class CalendarLinkFactory
     public function fromEvent(CalendarLinkableInterface $event): Link
     {
         return Link::create(
-            $event->getCalendarTitle(),
-            $event->getCalendarStartAt(),
-            $event->getCalendarEndAt(),
+            title: $event->getCalendarTitle(),
+            from: $event->getCalendarStartAt(),
+            to: $event->getCalendarEndAt(),
         )
             ->description($event->getCalendarDescription() ?? '')
             ->address($event->getCalendarAddress() ?? '')
