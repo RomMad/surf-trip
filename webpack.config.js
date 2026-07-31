53f654761eee87eaa79a663a55f73b7fc8bcad2d
RomMad <romain.madelaine@gmail.com>
2026-07-31 17:28:32 +0200

    refactor: migrate app and stimulus bootstrap files to TypeScript and update webpack configuration

--- webpack.config.js
+++ webpack.config.js
@@ -19,7 +19,7 @@ Encore
     * Each entry will result in one JavaScript file (e.g. app.js)
     * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
     */
-   .addEntry('app', './assets/app.js')
+   .addEntry('app', './assets/app.ts')
    .addAliases({
        'leaflet/dist/leaflet.min.css': 'leaflet/dist/leaflet.css',
    })
